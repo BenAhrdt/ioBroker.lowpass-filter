fad894510f6cfe1b7cbe814351b61a86834c627f
BenAhrdt <bsahrdt@gmail.com>
2022-02-20 13:20:02 +0100

    Initial commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+	"lowpass-filter adapter settings": {
+		"en": "Adapter settings for lowpass-filter",
+		"de": "Adaptereinstellungen für lowpass-filter",
+		"ru": "Настройки адаптера для lowpass-filter",
+		"pt": "Configurações do adaptador para lowpass-filter",
+		"nl": "Adapterinstellingen voor lowpass-filter",
+		"fr": "Paramètres d'adaptateur pour lowpass-filter",
+		"it": "Impostazioni dell'adattatore per lowpass-filter",
+		"es": "Ajustes del adaptador para lowpass-filter",
+		"pl": "Ustawienia adaptera dla lowpass-filter",
+		"zh-cn": "lowpass-filter的适配器设置"
+	},
+	"option1": {
+		"en": "option1",
+		"de": "Option 1",
+		"ru": "Опция 1",
+		"pt": "Opção 1",
+		"nl": "Optie 1",
+		"fr": "Option 1",
+		"it": "opzione 1",
+		"es": "Opción 1",
+		"pl": "opcja 1",
+		"zh-cn": "选项1"
+	},
+	"option2": {
+		"en": "option2",
+		"de": "Option 2",
+		"ru": "вариант 2",
+		"pt": "opção 2",
+		"nl": "Optie 2",
+		"fr": "Option 2",
+		"it": "opzione 2",
+		"es": "opcion 2",
+		"pl": "Opcja 2",
+		"zh-cn": "选项2"
+	}
+};
